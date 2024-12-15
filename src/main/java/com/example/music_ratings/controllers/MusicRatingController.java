beedc09ae5d6c7a6dac4cf4bdc020a804bf72d66
Kevin <kevinstill26@gmail.com>
2024-12-14 19:00:53 -0600

    springfox to openapi

--- src/main/java/com/example/music_ratings/controllers/MusicRatingController.java
+++ src/main/java/com/example/music_ratings/controllers/MusicRatingController.java
@@ -1,12 +1,21 @@
 package com.example.music_ratings.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class MusicRatingController {
 
+
     @RequestMapping("/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+    @RequestMapping("/hello")
     public String getMessage() {
         return "hello this is kevin!!";
     }
